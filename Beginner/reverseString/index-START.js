e8a6e7b1caed629fb9158211ddc4b5965af7792f
Emily Castillo <emilycastillox@gmail.com>
2022-06-02 23:34:48 -0400

    max recurring character

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -7,8 +7,60 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 function reverseString(text) {
     // Code goes here
+    return text.split("").reverse().join("")
 }
 
+//using spread operator in place of split - spreads characters into string of array elements
+
+function reverseString(text) {
+    return [...text].reverse().join('')
+}
+
+//for loop way: runs through string recieved and appends each character to another variable in reverse order
+
+function reverseString(text) {
+    let result = ''
+
+    for (let i = text.length -1; i >= 0; i--){
+        result += text[i]
+    }
+    return result
+}
+
+// for of loop: used to execute certain piece of code for each distinct item(propety) of an iterable object
+
+// runs through each character in the text recieved and append to beginning of a new variable, which we return as a reversed string
+
+function reverseString(text){
+    let result = "";
+
+    for(let char of text){
+        result = char + result
+    }
+    return result
+}
+
+// recursive way: reduce the problem into smaller instances of the same problem until it is completely solved.
+
+// uses .substr() method to return portion of text recieved. Two params, one starting index and number of chars after (optional)
+
+function reverseString(text) {
+    if(text === ""){ // terminal case - don't want endless loop
+        return ""
+    } else {
+        return reverseString(text.substr(1)) + text[0]
+    }
+}
+
+//reduce method: execute a function on every member of an array until it results in a single output value. It receives the function to be executed and the initial value of the accumulator as arguments. The accumulator serves as a holder for the value returned in the last execution of the callback.
+
+function reverseString(text) {
+    return text.split('').reduce((acc,char) => char + acc, '')
+}
+
+function reverseString(text) {
+    return [...text].reduce((acc, char) => char + acc, '')
+}
 
 
 module.exports = reverseString
--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,9 +3,21 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+//iterative approach
+
+const vowels = ['a','e','i','o','u']
 
 function vowelsCounter(text) {
-    // Code goes here
+    let counter = 0; //initialize counter
+
+    //loop through text to test if each character is a vowel
+    for (let letter of text.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            counter++
+        }
+    }
+    return counter
+
 }
 
 
